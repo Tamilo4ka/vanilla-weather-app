f570398df47c510e24c66b37e52781019cfe104d
Tamilo4ka <sone4ko2019@gmail.com>
2022-07-19 20:52:14 +0300

    Updated the CSS file

--- src/app.js
+++ src/app.js
@@ -68,7 +68,6 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "9f0294a92b6eb81ed3250921cf34f28c";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
